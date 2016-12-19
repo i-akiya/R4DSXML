700e450aadae3c7e3b9a904dffc7fd23939a3bf8
Ippei Akiya <ippei.aki@datadriven.jp>
2016-12-19 12:03:50 +0900

    Revert "def:ValueListRef support"

--- R4DSXML/R/getItemDef.R
+++ R4DSXML/R/getItemDef.R
@@ -23,9 +23,7 @@ getItemDef <- function( Nodeset ){
     ID_OriginDescription <- originList[[2]]
       
     #getVal(ItemDefNode4, 'TranslatedText[@xml:lang = "en"]')
-
-    ID_ValueListOID <- getValueListRef(ItemDefNode)
-
+    
     
     df <- data.frame(
         ID_OID,
@@ -39,7 +37,6 @@ getItemDef <- function( Nodeset ){
         ID_CodeListOID,
         ID_OriginType,
         ID_OriginDescription,
-        ID_ValueListOID,
         stringsAsFactors = FALSE
         )
       
